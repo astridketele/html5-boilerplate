7eec9f521ba3e01dbc914c98afa982b88ad2d97d
astridketele <keteleastrid@hotmail.com>
2017-05-16 12:11:16 +0200

    grid + image

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,27 @@
 console.log('Hello world!');
 
+var columnLeftMargin = null;
+var columnRightMargin = null;
+var boxedMarginBottom = null;
+
 $(document).ready(function() {
+
 	console.log('ready');
+
+	columnLeftMargin = parseFloat($('.column.left').css('margin-right'));
+	columnRightMargin = parseFloat($('.column.right').css('margin-left'));
+	boxedMarginBottom = columnLeftMargin + columnRightMargin;
+
+	$('.boxed, .column img').css({
+		'margin-bottom': boxedMarginBottom
+	});
+
 });
 
 $(window).resize(function() {
 	console.log('resize');
+	console.log('resize columnLeftMargin ', columnLeftMargin);
+	console.log('resize columnRightMargin ', columnRightMargin);
 });
 
 $(window).scroll(function() {
